fb0ee2d9b74798dc26358bd1090f0d6c4266b768
yzhizai <duansf@ihep.ac.cn>
2017-07-25 08:52:05 +0800

    find the most efficient code

--- src/matlab/main_loop.m
+++ src/matlab/main_loop.m
@@ -1,4 +1,4 @@
-function main
+function main_loop
 
 %% Choose the dwi image and diffeofield file
 dwiFile        = spm_select(1, 'nii', 'choose the diffusion MRI data');
@@ -15,12 +15,21 @@ bvalFile = spm_select(1, 'bval', 'bval', {}, pat);
 % Aff = rotationVectorToMatrix(pi/12*[0, 0, 1]);
 J = spm_diffeo('def2jac', Def);
 J = reshape(J, size(J, 1), size(J, 2), size(J, 3), []); 
-JCell = mat2cell(J, ones(1, size(J, 1)), ones(1, size(J, 2)), ones(1, size(J, 3)), ...
-    size(J, 4));
-RCell = cellfun(@jac2R, JCell, 'UniformOutput', false); % each element is a 3*3 matrix.
+% JCell = mat2cell(J, ones(1, size(J, 1)), ones(1, size(J, 2)), ones(1, size(J, 3)), ...
+%     size(J, 4));
+% RCell = cellfun(@jac2R, JCell, 'UniformOutput', false); % each element is a 3*3 matrix.
+R = zeros(size(J));
+for aa = 1:size(J, 1)
+    for bb = 1:size(J, 2)
+        for cc = 1:size(J, 3)
+            R(aa, bb, cc, :) = reshape(jac2R(J(aa, bb, cc, :)), [], 1);
+        end
+    end
+end
+
 % RCell = cellfun(@(x) reshape(x, 1, 1, 1, []), RCell, 'UniformOutput', false);
 % RMat = reshape(cat(1, RCell{:}), size(J, 1), size(J, 2), size(J, 3), []);
-clearvars J JCell
+clearvars J
 
 %% Fit DBFs and get n_b0
 
@@ -64,21 +73,30 @@ end
 b0_trans   = resample_based_deform(VF_dwi(1:n_b0), [], Def);
 % transform wMat 
 wMat_trans = resample_based_deform(VF_dwi(1), wMat, Def);
-[sizeX, sizeY, sizeZ, sizeS] = size(wMat_trans);
-wMat_trans_cell  = mat2cell(wMat_trans, ones(1, sizeX), ones(1, sizeY), ...
-    ones(1, sizeZ), sizeS);
+[sizeX, sizeY, sizeZ, ~] = size(wMat_trans);
+% wMat_trans_cell  = mat2cell(wMat_trans, ones(1, sizeX), ones(1, sizeY), ...
+%     ones(1, sizeZ), sizeS);
 
 %% For each voxel, affine matrix is unique, which is obtained from jacobian 
 % clear workspace
-clearvars wMat wMat_trans
+clearvars wMat
 
 
 % reconstruct the data into standard space.
 
-S_reg_cell = cellfun(@(x, y)compose_signal(bmatrix, n_b0, x, y),RCell, wMat_trans_cell, 'UniformOutput', false);
-clearvars RCell wMat_trans_cell
-S_reg_cell = cellfun(@(x) reshape(x, 1, 1, 1, []), S_reg_cell, 'UniformOutput', false);
-S_reg      = reshape(cat(1, S_reg_cell{:}), sizeX, sizeY, sizeZ, []);
+% S_reg_cell = cellfun(@(x, y)compose_signal(bmatrix, n_b0, x, y),RCell, wMat_trans_cell, 'UniformOutput', false);
+S_reg = zeros(sizeX, sizeY, sizeZ, numel(VF_dwi) - n_b0);
+for aa = 1:sizeX
+    for bb = 1:sizeY
+        for cc = 1:sizeZ
+            F = getDBFmatrix(bmatrix, n_b0, reshape(R(aa, bb, cc, :), 3, 3));
+            S_reg(aa, bb, cc, :) = F*reshape(wMat_trans(aa, bb, cc, :), [], 1);
+        end
+    end
+end
+% clearvars RCell wMat_trans_cell
+% S_reg_cell = cellfun(@(x) reshape(x, 1, 1, 1, []), S_reg_cell, 'UniformOutput', false);
+% S_reg      = reshape(cat(1, S_reg_cell{:}), sizeX, sizeY, sizeZ, []);
 S_reg      = cat(4, b0_trans, S_reg);
 
 ni     = nifti;
@@ -95,7 +113,6 @@ create(ni);
 for aa = 1:size(ni.dat, 4)
     ni.dat(:, :, :, aa) = S_reg(:, :, :, aa);
 end
-
-function S =  compose_signal(bmatrix, n_b0, Aff, w)
-F          = getDBFmatrix(bmatrix, n_b0, Aff);
-S          = F*w(:);
+% function S =  compose_signal(bmatrix, n_b0, Aff, w)
+% F          = getDBFmatrix(bmatrix, n_b0, Aff);
+% S          = F*w(:);
